794780027d71ec96d24481dae317db958552b54e
Diego Rascón <jose.d.rascon@gmail.com>
2025-04-15 23:27:32 -0700

    Netlify was configured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
-<a href="/about">About me</a>
+<h1>José Diego Rascón Amador</h1>
