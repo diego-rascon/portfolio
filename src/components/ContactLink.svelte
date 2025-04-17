8d48bce427eb80b6673e51990b0a53b88bc300ef
Diego Rascón <jose.d.rascon@gmail.com>
2025-04-16 23:30:20 -0700

    Component for contact links

---
+++ src/components/ContactLink.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let { children, href } = $props();
+</script>
+
+<a target="_blank" {href}>
+  {@render children?.()}
+</a>
+
+<style>
+  a {
+    flex: 1 1 0;
+    padding: 0.8rem 1.6rem;
+    text-decoration: none;
+    background-color: transparent;
+    border: 1px solid #01295f;
+    transition:
+      color 300ms,
+      background-color 300ms,
+      border-color 300ms;
+  }
+
+  a:hover {
+    color: #f4eded;
+    background-color: #01295f;
+    border-color: #01295f;
+  }
+</style>
