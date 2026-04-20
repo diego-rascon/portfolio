7527ebeb7c65fceb4667eabfd0d0b4a847602980
Diego Rascón <contact@drascon.com>
2026-04-20 03:18:42 -0700

    chore: moved and updated metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,15 @@
 	import Hero from '$lib/components/Hero.svelte';
 </script>
 
+<svelte:head>
+	<title>Diego Rascón</title>
+	<meta
+		name="description"
+		content="Full-Stack Engineer focused on modern web and mobile development. I bridge the gap between elegant client-side experiences and high-performance server architectures."
+	/>
+	<meta name="author" content="Diego Rascón" />
+</svelte:head>
+
 <!-- <NavBar /> -->
 <Hero />
 <main class="">
