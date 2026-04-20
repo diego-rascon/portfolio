12c1f0352311a17052d8573ecae65bde46230a53
Diego Rascón <contact@drascon.com>
2026-04-20 03:14:26 -0700

    feat: added a download resume button

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,5 +1,6 @@
 <script>
 	import ProfilePicture from '$lib/assets/images/profile-picture.jpg';
+	import Icon from '@iconify/svelte';
 </script>
 
 <section
@@ -11,10 +12,19 @@
 			Full-Stack Engineer focused on modern web and mobile development. I bridge the gap between
 			elegant client-side experiences and high-performance server architectures.
 		</p>
+		<a
+			rel="external"
+			href="/resume.pdf"
+			download="diego_rascon_resume.pdf"
+			class="mt-4 flex items-center justify-center gap-2 justify-self-start rounded-lg border px-4 py-2 text-gray-300 transition-colors duration-300 hover:text-sky-300"
+		>
+			<Icon icon="lucide:download" width="20" height="20" />
+			<span>Resume</span>
+		</a>
 	</div>
 	<enhanced:img
 		src={ProfilePicture}
-		alt="Profile picture"
+		alt="Diego Rascón"
 		class="size-64 rounded-2xl object-cover shadow-2xl md:my-48 md:size-80"
 	/>
 </section>
