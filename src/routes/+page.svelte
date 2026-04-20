ab10b2b316345d3423c9092619df87dc451803fc
Diego Rascón <contact@drascon.com>
2026-04-20 01:53:07 -0700

    refactor: divided page in components, commented out unfinished components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,13 @@
 <script>
-	import ContactLink from '$lib/components/ContactLink.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import Hero from '$lib/components/Hero.svelte';
 </script>
 
-<div
-	class="h-screen w-screen place-content-center bg-[#f4eded] p-8 text-center font-[Lexend] *:text-[#01295f]"
->
-	<header>
-		<h1 class="mb-2 text-4xl font-semibold">Diego Rascón</h1>
-		<p class="mb-6 text-xl font-light">Software Engineer</p>
-	</header>
+<!-- <NavBar /> -->
+<div class="container mx-auto">
+	<Hero />
 	<main>
-		<div class="mx-auto mt-2 flex max-w-3xl flex-col justify-center gap-4 font-light md:flex-row">
-			<ContactLink href="mailto:contact@drascon.com">contact@drascon.com</ContactLink>
-			<ContactLink href="https://github.com/diego-rascon">GitHub</ContactLink>
-			<ContactLink href="https://www.linkedin.com/in/diegorascon/">LinkedIn</ContactLink>
-		</div>
+		<!-- <Experience /> -->
 	</main>
+	<Footer />
 </div>
