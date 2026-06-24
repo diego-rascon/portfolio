4af27df7beee3c55dae635e0ecaa089fa834eabe
Diego Rascón <contact@drascon.com>
2026-06-23 21:29:44 -0700

    refactor: organized file structure of the layouts folder

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,7 +1,15 @@
-import AppLayout from "@/layouts/AppLayout.astro";
-import Footer from "@/layouts/footer/Footer.astro";
-import NavBar from "@/layouts/NavBar.astro";
+import AppLayout from "@/layouts/components/AppLayout.astro";
+import Footer from "@/layouts/components/footer/Footer.astro";
+import SocialLinks from "@/layouts/components/footer/SocialLinks.astro";
+import NavBar from "@/layouts/components/NavBar.astro";
 import type { NavItem } from "@/layouts/types/NavItem";
 import type { SocialLink } from "@/layouts/types/SocialLink";
 
-export { AppLayout, Footer, NavBar, type NavItem, type SocialLink };
+export {
+  AppLayout,
+  Footer,
+  NavBar,
+  SocialLinks,
+  type NavItem,
+  type SocialLink,
+};
