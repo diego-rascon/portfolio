48f9bf867a727613b4fc9ae6eb8f185e48434aef
Diego Rascón <contact@drascon.com>
2026-06-24 20:14:01 -0700

    chore: removed the astro-icon dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import icon from "astro-icon";
-
-import svelte from "@astrojs/svelte";
-
-import tailwindcss from "@tailwindcss/vite";
-
 import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), svelte()],
+  integrations: [svelte()],
 
   vite: {
     plugins: [tailwindcss()],
