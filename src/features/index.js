33899b141544d97c30958fed2d6399717118845f
Diego Rascón <contact@drascon.com>
2026-06-25 18:54:51 -0700

    feat: now the ProjectsSection component is exported

--- src/features/index.js
+++ src/features/index.js
@@ -1,6 +1,13 @@
 import CertificationsSection from "@/features/certifications/components/CertificationsSection.astro";
 import ExperienceSection from "@/features/experience/components/ExperienceSection.astro";
 import HeroSection from "@/features/hero/components/HeroSection.astro";
+import ProjectsSection from "@/features/projects/components/ProjectsSection.astro";
 import SkillsSection from "@/features/skills/components/SkillsSection.astro";
 
-export { CertificationsSection, ExperienceSection, HeroSection, SkillsSection };
+export {
+  CertificationsSection,
+  ExperienceSection,
+  HeroSection,
+  ProjectsSection,
+  SkillsSection,
+};
