9ab45f856e630b627d4e449c2d1e169e359e43ca
Diego Rascón <contact@drascon.com>
2026-06-30 18:45:53 -0700

    feat: now the list item used on certifications is a global component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import GlassContainer from "@/components/GlassContainer.astro";
 import GlowWrapper from "@/components/GlowWrapper.svelte";
+import ListItem from "@/components/ListItem.astro";
 import Section from "@/components/Section.astro";
 import StyledLink from "@/components/StyledLink.astro";
 import Subtitle from "@/components/Subtitle.astro";
 
-export { GlassContainer, GlowWrapper, Section, StyledLink, Subtitle };
+export { GlassContainer, GlowWrapper, ListItem, Section, StyledLink, Subtitle };
