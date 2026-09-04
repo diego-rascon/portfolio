3f7eb088fefda36a6b2839ae2137ea0a3e1dded4
Diego Rasón <contact@drascon.com>
2026-09-04 11:38:15 -0700

    feat: added support for crawlers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  site: "https://drascon.com",
+
+  integrations: [svelte(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
