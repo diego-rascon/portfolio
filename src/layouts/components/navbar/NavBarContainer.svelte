fd83c74153faf8f1441c6c6ea6b2f39e00c9e170
Diego Rasón <contact@drascon.com>
2026-09-04 12:12:16 -0700

    chore: remove phantom navbar class

--- src/layouts/components/navbar/NavBarContainer.svelte
+++ src/layouts/components/navbar/NavBarContainer.svelte
@@ -12,7 +12,7 @@
 <header class="z-10 fixed inset-x-0 top-0 px-4 w-full">
   <nav
     class={[
-      "flex justify-center md:justify-between mx-auto px-6 py-4 border-t rounded-full max-w-4xl font-semibold transition-all duration-300 navbar container",
+      "flex justify-center md:justify-between mx-auto px-6 py-4 border-t rounded-full max-w-4xl font-semibold transition-all duration-300",
       isScrolled
         ? "border-slate-700 shadow-lg backdrop-blur bg-slate-800/80 my-4 text-slate-300"
         : "my-8 md:my-12 text-slate-400 bg-slate-800/0 border-slate-700/0",
