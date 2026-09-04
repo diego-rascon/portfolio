423540655e1e61fdc60d8800e50ac45ea2411630
Diego Rasón <contact@drascon.com>
2026-09-04 12:05:22 -0700

    chore: removed year property and renamed createdAt to company

--- src/features/projects/types/Project.ts
+++ src/features/projects/types/Project.ts
@@ -2,9 +2,8 @@ import type { Skills } from "@/types/Skill";
 
 export interface Project {
   title: string;
-  createdAt?: string;
+  company: string;
   description: string;
-  year: number;
   url: string;
   image: string;
   skills: Skills[];
